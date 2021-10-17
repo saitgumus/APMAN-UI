c9f48c2f7f3e78b7c333db0caa6b7cdc56d56a5f
saitgumus <saitgumus33@gmail.com>
2021-10-17 18:31:03 +0300

    Updates

---
+++ precache-manifest.e591e97b1ef77f260871a780e869e231.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a02fdab2d7c4b2aaa0635454583ef52",
+    "url": "/apman-ui/index.html"
+  },
+  {
+    "revision": "480783a5864fca3ad405",
+    "url": "/apman-ui/static/css/2.0a55030d.chunk.css"
+  },
+  {
+    "revision": "480783a5864fca3ad405",
+    "url": "/apman-ui/static/js/2.d149a916.chunk.js"
+  },
+  {
+    "revision": "856c81d33e5c2d98728bb6cb87178017",
+    "url": "/apman-ui/static/js/2.d149a916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5934305567773f548a93",
+    "url": "/apman-ui/static/js/main.eda4f97a.chunk.js"
+  },
+  {
+    "revision": "62acfaf9d9cec9634b58",
+    "url": "/apman-ui/static/js/runtime-main.7e8d0ff1.js"
+  },
+  {
+    "revision": "7c0026da0fec21979aa1d0075f5f46dd",
+    "url": "/apman-ui/static/media/company-concept-illustration_114360-2581.7c0026da.jpg"
+  },
+  {
+    "revision": "98771346e83f085ae8c10372e6396114",
+    "url": "/apman-ui/static/media/group-business.98771346.jpg"
+  },
+  {
+    "revision": "e53846461b47fd5b42b19cad0545d39a",
+    "url": "/apman-ui/static/media/individual.e5384646.jpg"
+  }
+]);
