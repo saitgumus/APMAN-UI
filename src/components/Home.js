8b9103663380161280414312a990f4e18c1405ce
saitgumus <saitgumus33@gmail.com>
2021-10-19 10:51:53 +0300

    dummy checkin.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import { CommonTypes } from "../Types/Common";
 import * as pageActions from "../redux/actions/page-actions";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+import { GetActiveLocalUser } from "../Core/Helper";
 
 class Home extends Component {
   static displayName = Home.name;
@@ -20,7 +21,9 @@ class Home extends Component {
       <div>
         <Jumbotron fluid>
           <Container fluid>
-            <h1 className="display-3">Merhaba</h1>
+            <h3 className="display-3">
+              Merhaba {GetActiveLocalUser().firstName}
+            </h3>
             <p className="lead">Site yönetim uygulamasına hoş geldin...</p>
           </Container>
         </Jumbotron>
